90127071bb26690d00bf36175d5193e26680f5c3
Sabia Tahir <sabiatahir@hotmail.com>
2021-01-14 19:22:40 +0000

    Added egde case for error

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,16 +5,20 @@ const breed = process.argv.splice(2)[0];
 // console.log(breed);
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  const data = JSON.parse(body);
   if (body === '[]') {
     console.log('The requested breed is not found');
-    process.exit()
+    process.exit();
   }
+  if (error) {
+    console.log(`Error: ${error}`);
+    process.exit();
+  }
+  const data = JSON.parse(body);
   // console.log(body); // Print the entire response body sent back from API server
   // console.log(typeof body);
-   // converts the string into object so we can scan it
+  // converts the string into object so we can scan it
   // console.log(data);
   // console.log(typeof data);
-  console.log(data[0].description);
+  console.log(data[0].description); //Will print out description from the data object
 });
 
